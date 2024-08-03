b290976dd1ea8abcd4fcc0443d409d5d55bdeded
thisapaydown <thisapaydown@outlook.com>
2024-08-03 08:18:02 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "def.h"
+#include <iostream>
+#include <stdio>
+#include <string>
+#include <time>
+
+using std:
+
+int main(){
+  int cnd = 120 ;
+  printf("Welcome %d", cnd) ;
+  return 0 ;
+}
