f9ca25176e21f121a96a7a8a8e00ce331d217679
thisapaydown <thisapaydown@outlook.com>
2024-08-03 08:18:55 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pyautogui as pag
+import subprocess
+import time
+
+exe_path = "vpnplanet.exe"
+
+
+# Run the executable
+subprocess.Popen([exe_path])
+
+
+actions = [
+    (611,528,4),
+    (611,528,2),
+    (611,528,2),
+    (611,528,2),
+    (611,528,2),
+    (611,528,2),
+    (625,371,36),
+    (626,531,5),
+    (500,531,26),
+    (427,479,12),
+    (629,434,6),
+    (856,156,36),
+]
+
+for x, y, duration in actions:
+    pag.click(x, y, duration=duration)
+
+# this is content is ppp2a and ppp@pa
+#hashing may contains error 
+# let it sign out 
